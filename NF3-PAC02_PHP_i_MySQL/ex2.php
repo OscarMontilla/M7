692d22b0c68b732e10d8cc5c2ecdc513651f01f8
OscarMontilla <120601904+OscarMontilla@users.noreply.github.com>
2023-10-05 22:28:12 +0200

    Update ex2.php

--- NF3-PAC02_PHP_i_MySQL/ex2.php
+++ NF3-PAC02_PHP_i_MySQL/ex2.php
@@ -1,12 +1,11 @@
 <?php
-// connect to mysqli
+
 $db = mysqli_connect('localhost', 'root', 'root') or 
     die ('Unable to connect. Check your connection parameters.');
 
-//make sure you're using the correct database
 mysqli_select_db($db,'moviesite') or die(mysqli_error($db));
 
-// insert data into the movie table
+
 $query = 'INSERT INTO movie
         (movie_id, movie_name, movie_type, movie_year, movie_leadactor,
         movie_director)
